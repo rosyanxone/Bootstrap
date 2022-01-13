7138a2c6101cbb661c040d15915984694f876c7f
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2022-01-13 15:41:44 +0800

    Smooth Scroll

--- js/script.js
+++ js/script.js
@@ -17,8 +17,28 @@
 
 // });
 
+
 // -- PUNYA YT TRAVERSY MEDIA ---
+// $('.page-scroll').on('click', function(e) {
 
-$('.page-scroll').on('click', function(e) {
+//     if (this.hash !== '') {
+//         e.preventDefault();
 
-});
+//         const hash = this.hash;
+
+//         $('html, body').animate({
+//             scrollTop: $(hash).offset.top
+//         }, 800);
+//     }
+// });
+
+
+// -- PUNYA YT DEV ED ---
+function smoothScroll(target, duration) {
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect();
+
+    console.log(targetPosition);
+}
+
+smoothScroll('.page-scroll', 1000);
