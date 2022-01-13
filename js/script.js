fcec9a1ee0fb9d5d5d69206783ea566fd99a5c69
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2022-01-13 15:20:52 +0800

    Smooth Scroll

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
-// Event pada link di klik
+// --- PUNYA PAK DHIKA ---
+// // Event pada link di klik
+// $('.page-scroll').on('click', function(e) {
+
+//     // Ambil isi href
+//     var tujuan = $(this).attr('href');
+
+//     // Tangkap elemen ybs
+//     var elemenTujuan = $(tujuan);
+
+//     // Pindahkan scroll
+//     $('body').animate({
+//         scrollTop: elemenTujuan.offset().top - 99
+//     }, 1000, 'linier');
+
+//     e.preventDefault();
+
+// });
+
+// -- PUNYA YT TRAVERSY MEDIA ---
+
 $('.page-scroll').on('click', function(e) {
-
-    // Ambil isi href
-    var tujuan = $(this).attr('href');
-
-    // Tangkap elemen ybs
-    var elemenTujuan = $(tujuan);
-
-    // Pindahkan scroll
-    $('body').animate({
-        scrollTop: elemenTujuan.offset().top - 99
-    }, 1000, 'linier');
-
-    e.preventDefault();
 
 });
